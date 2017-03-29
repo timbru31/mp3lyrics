f20b140708c2b492e887e2899a4401145e4c02d8
Tim Brust <tim.brust@sinnerschrader.com>
2017-03-29 14:41:56 +0200

    fix: spelling

--- lib/genius.rb
+++ lib/genius.rb
@@ -2,8 +2,8 @@ require_relative './wiki'
 
 # Fetches the lyrics from Genius (formerly known as Rap Genius)
 # Lyrics are stored accessed via the URL schema https://genius.com/ARTIST-SONG-lyrics
-# They are inside //lyircs[@class="lyrics"]//p[1], a lyrics element with the class "lyrics"
-# It seems that this element was added by Anglar.js
+# They are inside //lyrics[@class="lyrics"]//p[1], a lyrics element with the class "lyrics"
+# It seems that this element was added by Angular.js
 # Inside the p in the lyrics div they are in a tags
 class Genius < Wiki
   def get_lyrics(artist, song, limit = 10)
