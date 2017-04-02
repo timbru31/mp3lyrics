0bf92bbd29d802697622e2a699dd89b8c7c382e1
xGhOsTkiLLeRx <ratchet.player@gmx.de>
2017-04-02 20:39:38 +0200

    test: update genius test, skip azlyrics for Travis, send coverage reports from CircleCI

--- spec/genius_spec.rb
+++ spec/genius_spec.rb
@@ -3,12 +3,12 @@ require_relative '../lib/genius'
 
 numb_lyrics = "[Verse 1]\rI'm tired of being what you want me to be\rFeeling so faithless, lost "\
   "under the surface\rDon't know what you're expecting of me\rPut under the pressure of walking "\
-  "in your shoes\r\r[Pre-Hook]\r(Caught in the undertow, just caught in the undertow)\rEvery step that I take "\
+  "in your shoes\r\r[Pre-Hook 1]\r(Caught in the undertow, just caught in the undertow)\rEvery step that I take "\
   "is another mistake to you\r(Caught in the undertow, just caught in the undertow)\r\r[Hook]\r"\
   "I've become so numb, I can't feel you there\rBecome so tired, so much more aware\rI'm "\
   "becoming this, all I want to do\rIs be more like me and be less like you\r\r[Verse 2]\rCan't "\
   "you see that you're smothering me?\rHolding too tightly, afraid to lose control?\r'Cause "\
-  "everything that you thought I would be\rHas fallen apart right in front of you\r\r[Pre-Hook]\r(Caught in "\
+  "everything that you thought I would be\rHas fallen apart right in front of you\r\r[Pre-Hook 2]\r(Caught in "\
   "the undertow, just caught in the undertow)\rEvery step that I take is another mistake to you\r"\
   "(Caught in the undertow, just caught in the undertow)\rAnd every second I waste is more than "\
   "I can take\r\r[Hook]\rI've become so numb, I can't feel you there\rBecome so tired, so much "\
