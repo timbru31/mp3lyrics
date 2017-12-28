48318fe9f673a8b39635acc560b4d1b6aff75837
Tim Brust <ratchet.player@gmx.de>
2017-12-28 21:31:53 +0100

    test: update lyrics for Numb, test against Ruby 2.5, silence Snyk

--- Gemfile
+++ Gemfile
@@ -6,14 +6,14 @@ gem 'require_all', '>=1.4.0'
 gem 'ruby-mp3info', '>=0.8.10'
 
 group :development do
-  gem 'pry-byebug', '>=3.5.0', require: false
-  gem 'rubocop', '>=0.51.0'
+  gem 'pry-byebug', '>=3.5.1', require: false
+  gem 'rubocop', '>=0.52.1'
 end
 
 group :test do
   gem 'codeclimate-test-reporter', git: 'https://github.com/timbru31/ruby-test-reporter.git', require: false
-  gem 'coveralls_reborn', '~> 0.9.0', require: false
+  gem 'coveralls_reborn', '~> 0.10.0', require: false
   gem 'minitest', '>=5.10.3'
-  gem 'rake', '>=12.2.1'
+  gem 'rake', '>=12.3.0'
   gem 'simplecov', '>=0.15.1', require: false
 end
--- spec/genius_spec.rb
+++ spec/genius_spec.rb
@@ -1,24 +1,25 @@
 require_relative './spec_helper'
 require_relative '../lib/genius'
 
-numb_lyrics = "[Verse 1]\rI'm tired of being what you want me to be\rFeeling so faithless, lost "\
+numb_lyrics = "[Verse 1: Chester Bennington]\rI'm tired of being what you want me to be\rFeeling so faithless, lost "\
   "under the surface\rDon't know what you're expecting of me\rPut under the pressure of walking "\
-  "in your shoes\r\r[Pre-Chorus 1]\rCaught in the undertow, just caught in the undertow\rEvery step that I take "\
-  "is another mistake to you\rCaught in the undertow, just caught in the undertow\r\r[Chorus]\r"\
+  "in your shoes\r\r[Pre-Chorus: Chester &amp; Mike Shinoda]\rCaught in the undertow, just caught "\
+  "in the undertow\rEvery step that I take "\
+  "is another mistake to you\rCaught in the undertow, just caught in the undertow\r\r[Chorus: Chester]\r"\
   "I've become so numb, I can't feel you there\rBecome so tired, so much more aware\rI'm "\
-  "becoming this, all I want to do\rIs be more like me and be less like you\r\r[Verse 2]\rCan't "\
+  "becoming this, all I want to do\rIs be more like me and be less like you\r\r[Verse 2: Chester]\rCan't "\
   "you see that you're smothering me?\rHolding too tightly, afraid to lose control?\r'Cause "\
-  "everything that you thought I would be\rHas fallen apart right in front of you\r\r[Pre-Chorus 2]\rCaught in "\
-  "the undertow, just caught in the undertow\rEvery step that I take is another mistake to you\r"\
+  "everything that you thought I would be\rHas fallen apart, right in front of you\r\r[Pre-Chorus: Chester &amp; "\
+  "Mike]\rCaught in the undertow, just caught in the undertow\rEvery step that I take is another mistake to you\r"\
   "Caught in the undertow, just caught in the undertow\rAnd every second I waste is more than "\
-  "I can take\r\r[Chorus]\rI've become so numb, I can't feel you there\rBecome so tired, so much "\
+  "I can take\r\r[Chorus: Chester]\rI've become so numb, I can't feel you there\rBecome so tired, so much "\
   "more aware\rI'm becoming this, all I want to do\rIs be more like me and be less like you\r\r"\
-  "[Bridge]\rAnd I know, I may end up failing too\rBut I know, you were just like me\r"\
-  "With someone disappointed in you\r\r[Chorus]\rI've become so numb, I can't feel you there\r"\
+  "[Bridge: Chester]\rAnd I know, I may end up failing, too\rBut I know, you were just like me\r"\
+  "With someone disappointed in you\r\r[Chorus: Chester]\rI've become so numb, I can't feel you there\r"\
   "Become so tired, so much more aware\rI'm becoming this, all I want to do\rIs be more like me "\
-  "and be less like you\r\r[Outro]\rI've become so numb, I can't feel you there\r(I'm tired of "\
-  "being what you want me to be)\rI've become so numb, I can't feel you there\r(I'm tired of "\
-  'being what you want me to be)'
+  "and be less like you\r\r[Outro: Chester]\rI've become so numb, I can't feel you there\rI'm tired of "\
+  "being what you want me to be\rI've become so numb, I can't feel you there\rI'm tired of "\
+  'being what you want me to be'
 
 describe Genius, 'Lyrics fetching' do
   before do
