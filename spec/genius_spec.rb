93db83e194f43ce38bbe73427e552448ce537d77
Tim Brust <tim.brust@sinnerschrader.com>
2018-07-30 16:19:45 +0200

    test: update lyrics

--- spec/genius_spec.rb
+++ spec/genius_spec.rb
@@ -1,20 +1,20 @@
 require_relative './spec_helper'
 require_relative '../lib/genius'
 
-numb_lyrics = "[Verse 1: Chester Bennington]\rI'm tired of being what you want me to be\rFeeling so faithless\rLost under the surface\r" \
-  "Don't know what you're expecting of me\rPut under the pressure\rOf walking in your shoes\r\r[Pre-Chorus 1: Chester Bennington &amp; " \
-  "Mike Shinoda]\rCaught in the undertow\rJust caught in the undertow\rEvery step that I take is\rAnother mistake to you\rCaught in the " \
-  "undertow\rJust caught in the undertow\r\r[Chorus: Chester Bennington]\rI've\rBecome so numb\rI can't feel you there\rBecome so tired" \
-  "\rSo much more aware\rI'm becoming this\rAll I want to do\rIs be more like me\rAnd be less like you!\r\r[Verse 2: Chester Bennington]" \
-  "\rCan't you see that you're smothering me?\rHolding too tightly\rAfraid to lose control\r'Cause everything that you thought I would be" \
-  "\rHas fallen apart, right in front of you\r\r[Pre-Chorus 2: Chester Bennington &amp; Mike Shinoda]\rCaught in the undertow\rJust " \
-  "caught in the undertow\rEvery step that I take is\rAnother mistake to you\rCaught in the undertow\rJust caught in the undertow\rAnd " \
-  "every second I waste is more than I can take!\r\r[Chorus: Chester Bennington]\rI've\rBecome so numb\rI can't feel you there\rBecome so" \
-  " tired\rSo much more aware\rI'm becoming this\rAll I want to do\rIs be more like me\rAnd be less like you!\r\r[Bridge: Chester " \
-  "Bennington]\rAnd I know\rI may end up failing, too\rBut I know\rYou were just like me\rWith someone disappointed in you\r\r[Chorus: " \
-  "Chester Bennington]\rI've\rBecome so numb\rI can't feel you there\rBecome so tired\rSo much more aware\rI'm becoming this\rAll I want" \
-  " to do\rIs be more like me\rAnd be less like you!\r\r[Outro: Chester Bennington]\rI've\rBecome so numb\r(I can't feel you there)\rI'm" \
-  " tired of being what you want me to be\rI've\rBecome so numb\r(I can't feel you there)\rI'm tired of being what you want me to be"
+numb_lyrics = "[Verse 1: Chester Bennington]\rI'm tired of being what you want me to be\rFeeling so faithless, lost under the surface\r" \
+  "Don't know what you're expecting of me\rPut under the pressure of walking in your shoes\r\r[Pre-Chorus: Chester Bennington &amp; " \
+  "Mike Shinoda]\rCaught in the undertow\rJust caught in the undertow\rEvery step that I take is another mistake to you\rCaught in the " \
+  "undertow\rJust caught in the undertow\r\r[Chorus: Chester Bennington]\rI've become so numb, I can't feel you there\rBecome so tired" \
+  ", so much more aware\rI'm becoming this, all I want to do\rIs be more like me and be less like you!\r\r[Verse 2: Chester Bennington]" \
+  "\rCan't you see that you're smothering me?\rHolding too tightly, afraid to lose control\r'Cause everything that you thought I would be" \
+  "\rHas fallen apart, right in front of you\r\r[Pre-Chorus: Chester Bennington &amp; Mike Shinoda]\rCaught in the undertow\rJust " \
+  "caught in the undertow\rEvery step that I take is another mistake to you\rCaught in the undertow\rJust caught in the undertow\rAnd " \
+  "every second I waste is more than I can take\r\r[Chorus: Chester Bennington]\rI've become so numb, I can't feel you there\rBecome so" \
+  " tired, so much more aware\rI'm becoming this, all I want to do\rIs be more like me and be less like you!\r\r[Bridge: Chester " \
+  "Bennington]\rAnd I know I may end up failing, too\rBut I know you were just like me\rWith someone disappointed in you\r\r[Chorus: " \
+  "Chester Bennington]\rI've become so numb, I can't feel you there\rBecome so tired, so much more aware\rI'm becoming this, all I want" \
+  " to do\rIs be more like me and be less like you!\r\r[Outro: Chester Bennington]\rI've become so numb, I can't feel you there\rI'm" \
+  " tired of being what you want me to be\rI've become so numb, I can't feel you there\rI'm tired of being what you want me to be"
 
 describe Genius, 'Lyrics fetching' do
   before do
