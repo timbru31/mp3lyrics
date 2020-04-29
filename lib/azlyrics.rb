31cf4ce9e8a9ed34fec30784f0f40cfec6337e77
Tim Brust <github@timbrust.de>
2020-04-29 13:03:20 +0000

    debug: logs AZYLyrics body

--- lib/azlyrics.rb
+++ lib/azlyrics.rb
@@ -11,6 +11,7 @@ class AZLyrics < Wiki
 
     res = fetch("https://www.azlyrics.com/lyrics/#{artist.downcase}/#{song.downcase}.html", limit)
     puts res.code
+    puts res.body
     return nil unless res.is_a? Net::HTTPSuccess
 
     lyrics = Nokogiri::HTML(res.body).xpath('/html/body/div[2]/div/div[2]/div[5]')
